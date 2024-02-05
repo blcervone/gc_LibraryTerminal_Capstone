56dcc5aea0fb4c198ab8df7eb10648618c43b27a
Logan Duvick <loganduvick@gmail.com>
2024-02-05 10:00:16 -0500

    class def book added Logan

---
+++ Book.py
@@ -0,0 +1,26 @@
+# Your solution must include some kind of a book class with a title, author, status, condition
+# and due date if checked out.
+# Status should be On Shelf or Checked Out (or other statuses you can imagine).
+# Condition will represent the wear and tear of the book,
+# and will degrade each time the book is checked out. Use any data type you wish to repress this.
+# If the book is too degraded, when it is returned it will be recycled.
+from datetime import datetime, timedelta
+import random as rand
+class Book:
+    def __init__(self, title, author, status, condition, due_date):
+        self.title = title
+        self.author = author
+        self.status = status
+        self.condition = condition
+        self.due_date = due_date
+
+    def set_due_date(self):
+        self.due_date = datetime.now()+timedelta(days=14)
+
+    def degrade(self):
+        degrade_num=rand.randint(1, 5)
+        self.condition -= degrade_num
+
+    def __str__(self):
+        return f'Title:{self.title} \n Author: {self.author} \n Status: {self.status} \n Condition: {self.condition} \n Due Date: {self.due_date}'
+
